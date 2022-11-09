0db0b6aab2c78371fe0e4f0210436b6ccbb276f3
digabro <100244869+digabro@users.noreply.github.com>
2022-11-09 11:44:16 -0700

    few small changes
    +food effect time for how many turns it gives the effect
    +addfood function

--- food.h
+++ food.h
@@ -7,8 +7,12 @@
 class Food : public Inventory{
 private:
     string Effect_;
+    int EffectTime_;
 public:
+    addFood(string ItemName_,string ItemType_,int ItemCost_,int ItemIndex_,string Effect_,int EffectTime_,);//add a food item
     string getEffect(int ItemIndex_);
+    int getEffectTime(int ItemIndex_);
     void setEffect(int ItemIndex_);
-
+    void setEffectTime(int ItemIndex_);
+    
 };
