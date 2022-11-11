b5a08ba15a493036a8d8954c78e424729fb4d519
Lachlankotarski <lako2765@colorado.edu>
2022-11-10 23:19:15 -0700

    copy inventory asserts

--- testDriver.cpp
+++ testDriver.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-// #include "Inventory.h"
-#include "Item.h"
+#include "Inventory.h"
+// #include "Item.h"
 // #include "location.h"
 // #include "person.h"
 // #include "npc.h"
 // #include "player.h"
-
+#include <cassert>
 
 using namespace std;
  
 // sorting Algorithm
-void sort(int arr[], int n)
+void sort(int arr[], int arr_size)
 {
-    for (int i = 0; i < n - 1; i++){
-        for (int j = 0; j < n - i - 1; j++){
+    for (int i = 0; i < arr_size - 1; i++){
+        for (int j = 0; j < arr_size - i - 1; j++){
             if (arr[j] > arr[j + 1]){
                 swap(arr[j], arr[j + 1]);
             }
@@ -22,20 +22,61 @@ void sort(int arr[], int n)
 }
 
 int main(){
-    int n=15;
+    int arr_size = 15;
     int arr[] = {7,8,424,6,3,1,5,1,98,10};
     sort(arr,15);
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < arr_size; i++)
     {
-        //cout << arr[i] << endl;
+        cout << arr[i] << endl;
     }
-    Item testItem = Item();
-    cout<<testItem.getDamage()<<endl;
-    // Player test=Player();
 
-    /*
-    Sorry 
+    // Inventory
+    Inventory testInventory = Inventory(250, 1, 2, 0, 4);
+    assert(testInventory.getBuffBucks() == 250);
+    assert(testInventory.getPedialyte() == 1);
+    assert(testInventory.getEnergyDrink() == 2);
+    assert(testInventory.getMuscleMilk() == 0);
+    assert(testInventory.getCupOfNoodles() == 4);
+
+    //setters
+    testInventory.setBuffBucks(255);
+    if(testInventory.getBuffBucks() == 255)
+    {
+        cout << "1" << endl;
+    }
+    else
+    {
+        cout << "-1" << endl;
+    }
+    /*Expected 
+    num_buff_bucks = 255
     */
+    testInventory.setEnergyDrink(3);
+    if(testInventory.getEnergyDrink() == 2)
+    {
+        cout << "1" << endl;
+    }
+    else
+    {
+        cout << "-1" << endl;
+    }
+    /*Expected 
+    num_energy_drink = 2;
+    */
+
+    //Inventory > item setter
+    
+    
+
+
+    // Item 
+    Item testItem = Item();
+    cout << testItem.getDamage() << endl;
+
+
+    // location 
+    
+    
 
 return 0;
 }
