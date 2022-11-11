91d7e5ef336c8930da7dfa6b52d3964eb389ac4d
digabro <100244869+digabro@users.noreply.github.com>
2022-11-10 17:37:04 -0700

    removed unneeded classes added location

---
+++ location.h
@@ -0,0 +1,17 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+#include <iostream>
+#include <vector>
+#include <string>
+#using namespace std;
+
+class Location{
+    private:
+        string name_;
+        int difficulty_;
+        char symbol;
+    public:
+
+};
+
+#endif
