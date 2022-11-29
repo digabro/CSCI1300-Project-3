3cf5f2f0bbc77e2a44cbf0d7434372650abcea2a
Lachlankotarski <lako2765@colorado.edu>
2022-11-28 19:21:19 -0700

    writing files

---
+++ colleges.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int split(int j)
+{
+    //ex
+    return j;
+}
+
+void printCollegeQuestions(string file_name)
+{
+    ifstream inFile;
+    inFile.open(file_name);
+    string line;
+
+    //extra variables
+    
+    // while(getline(inFile, line))
+    // {
+    //     if()
+    // }
+
+
+
+
+
+    return;
+}
+
+
+
+
+
+int main(){
+
+    string compSci[2][7] ={ {"h","e","l","l","o"}, {"W","O","R","L","D"}};
+
+
+
+
+
+    return 0;
+}3
